7099b5b27652c112475fbbbb0bd7d1400174a42d
Nihal Saxena <saxenanihal95@gmail.com>
2019-12-18 09:02:48 +0530

    Moved styles to constants

--- components/GithubUser.js
+++ components/GithubUser.js
@@ -6,45 +6,32 @@ import {
   Dimensions,
   FlatList,
   ScrollView,
+  StyleSheet,
 } from 'react-native';
 
 function GithubRepo(props) {
   return (
-    <View style={{padding: 20}}>
+    <View style={styles.githubRepoItemContainer}>
       <Text>{props.name}</Text>
     </View>
   );
 }
 
 export default class GithubUser extends Component {
-  renderSeparator = () => (
-    <View
-      style={{
-        backgroundColor: 'black',
-        height: 0.5,
-      }}
-    />
-  );
+  renderSeparator = () => <View style={styles.itemSeperator} />;
   render() {
     const {user: {avatar_url = '', name = ''} = {}, repos} = this.props;
-    const {width} = Dimensions.get('window');
     return (
       <ScrollView>
-        <View style={{alignItems: 'center', marginVertical: 10}}>
+        <View style={styles.githubRepoUserViewContainer}>
           <Image
             source={{uri: avatar_url}}
-            style={{width: width / 2, height: 200}}
+            style={styles.githubAvatarStyle}
             resizeMode={'cover'}
           />
           <Text>{name}</Text>
         </View>
-        <View
-          style={{
-            height: 30,
-            backgroundColor: 'gray',
-            justifyContent: 'center',
-            paddingHorizontal: 10,
-          }}>
+        <View style={styles.repositoryTextContainer}>
           <Text>Repositories</Text>
         </View>
         <FlatList
@@ -58,3 +45,29 @@ export default class GithubUser extends Component {
     );
   }
 }
+
+const {width} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  itemSeperator: {
+    backgroundColor: 'black',
+    height: 0.5,
+  },
+  repositoryTextContainer: {
+    height: 30,
+    backgroundColor: 'gray',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  githubRepoItemContainer: {
+    padding: 20,
+  },
+  githubRepoUserViewContainer: {
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  githubAvatarStyle: {
+    width: width / 2,
+    height: 200,
+  },
+});
